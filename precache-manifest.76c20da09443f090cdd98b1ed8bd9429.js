2aac0597ce3bd6f7a8c6d807c5a9116fc484df7a
jackkower <jackkower@gmail.com>
2020-08-06 06:58:59 -0700

    Updates

---
+++ precache-manifest.76c20da09443f090cdd98b1ed8bd9429.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55aaabca8d5e349d55ea4b2dd68a5792",
+    "url": "/cityscape/index.html"
+  },
+  {
+    "revision": "c4303202b9033251804e",
+    "url": "/cityscape/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d3f7809ebbc72d240565",
+    "url": "/cityscape/static/css/main.5f19bee2.chunk.css"
+  },
+  {
+    "revision": "c4303202b9033251804e",
+    "url": "/cityscape/static/js/2.a19f8633.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/cityscape/static/js/2.a19f8633.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3f7809ebbc72d240565",
+    "url": "/cityscape/static/js/main.e5a3f4f5.chunk.js"
+  },
+  {
+    "revision": "76a6218e8a93329b6939",
+    "url": "/cityscape/static/js/runtime-main.f07a90c0.js"
+  },
+  {
+    "revision": "b57bcb6cd1b5694d08b46619c42f92f4",
+    "url": "/cityscape/static/media/SD.b57bcb6c.jpg"
+  },
+  {
+    "revision": "7d76f88dc3f5fe5b326c3d255a0a6faa",
+    "url": "/cityscape/static/media/chig.7d76f88d.jpg"
+  },
+  {
+    "revision": "47d2bbf1f4867982182195d47d560a10",
+    "url": "/cityscape/static/media/hipster.47d2bbf1.jpg"
+  },
+  {
+    "revision": "ef78ff75f07ab569af70185a411549b6",
+    "url": "/cityscape/static/media/jackrec.ef78ff75.jpg"
+  },
+  {
+    "revision": "de9628846cba038b8f9ac1c003a86425",
+    "url": "/cityscape/static/media/nyc.de962884.jpg"
+  },
+  {
+    "revision": "f50286185fe19d7ef937b27612ad6a7d",
+    "url": "/cityscape/static/media/testi.f5028618.JPG"
+  }
+]);
